cb87547916d1c39e1f6d491fa04b63663fb074a9
Moises Hernandez <mhdezcoronado@gmail.com>
2023-02-23 17:35:19 +0100

    Add device for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   namespace :api do
     namespace :v1 do
-      resources :projects, only: [:index, :show, :create, :update, :destroy]
+      resources :projects, only: %i[index show create update destroy]
     end
   end
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'api/v1/projects#index'
 end
