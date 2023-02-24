1bc51d1d569da56f769beb18f50b949833de495a
Moises Hernandez <mhdezcoronado@gmail.com>
2023-02-24 19:47:56 +0100

    Remove unnessessary files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::API
   # include ActionController::MimeResponds
   # respond_to :json
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # protected
+  # # protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email password role])
-  end
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: %i[email password role])
+  # end
 end
