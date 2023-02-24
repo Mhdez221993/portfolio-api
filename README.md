962aa9bffdd7079664566ac83d66a71da8b816ee
Moises Hernandez <mhdezcoronado@gmail.com>
2023-02-24 17:18:15 +0100

    Restric the numbers of admin

--- README.md
+++ README.md
@@ -1,24 +1,5 @@
-# README
+## Links
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
-
-Things you may want to cover:
-
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+* ![devise](https://github.com/heartcombo/devise)
+* ![cancancan](https://github.com/CanCanCommunity/cancancan)
+* ![Devise and JWT](https://dakotaleemartinez.com/tutorials/devise-jwt-api-only-mode-for-authentication/)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :projects
 
+  MAX_ADMINS = 1
+
   include Devise::JWT::RevocationStrategies::JTIMatcher
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -9,8 +11,15 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true, length: { minimum: 8 }
+  validate :admin_quota, on: :create
 
   def admin?
     role == 'admin'
+  end
+
+  def admin_quota
+    return unless admin? && User.where(role: :admin).count >= MAX_ADMINS
+
+    errors.add(:role, 'exceeded maximum number of admins')
   end
 end
